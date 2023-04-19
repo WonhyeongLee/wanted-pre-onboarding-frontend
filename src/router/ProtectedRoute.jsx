4035b10c4aa3e2da6eb79653f7f2afb81450807c
WonhyeongLee <bigfile57@naver.com>
2023-04-19 19:30:48 +0900

    Fix: 요구사항에 맞춰서 /todo 페이지에서 로그인 토큰이 없을 때 / 대신 /signin으로 이동하게 수정

--- src/router/ProtectedRoute.jsx
+++ src/router/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = localStorage.getItem("access_token") !== null;
-  return isAuthenticated ? children : <Navigate to='/' replace />;
+  return isAuthenticated ? children : <Navigate to='/signin' replace />;
 };
 
 export default ProtectedRoute;
